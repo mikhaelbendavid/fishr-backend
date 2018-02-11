61b00e37b05bfcbcf1e9002d15b25577ce6d7960
Michael Litchev <director@mikhaelbd.com>
2018-02-10 16:12:40 -0800

    Added express server, server folder organization

---
+++ app.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const cors = require('cors');
+app.use(logger('dev'));
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to Fishr'
+}));
+
+module.exports = app;
